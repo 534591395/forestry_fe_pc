4c638f9618c30d73143e43e16f8070bf0836ff44
534591395@qq.com <534591395@qq.com>
2019-05-06 23:51:13 +0800

    【MOD】编译

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.01b3e18ac10986415024f8a70b212fec.js"
+  "/precache-manifest.e27c86737c7657d1ec854fcae62b12ef.js"
 );
 
 workbox.clientsClaim();
