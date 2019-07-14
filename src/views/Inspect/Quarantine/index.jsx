c2c57cb47b0a1d2c63db3da910a955c4358fb98a
534591395@qq.com <534591395@qq.com>
2019-07-14 12:31:59 +0800

    【MOD】定期审核套接口

--- src/views/Inspect/Quarantine/index.jsx
+++ src/views/Inspect/Quarantine/index.jsx
@@ -5,11 +5,79 @@ import './index.less';
 
 class Quarantine extends Component {
   state = {
-    tableData: []
+    page: {
+      current: 1,
+      size: 10,
+      total: 0
+    },
+    tableData: [],
+    imageModal: false,
+    imageList: [
+      {
+        title: '',
+        images: []
+      }
+    ],
+    loading: false
   }
 
   componentDidMount() {
+    this.getList();
+  }
 
+  getList = (data, isSearch) => {
+    this.setState({
+      loading: true
+    });
+    if (isSearch) {
+      let page = this.state.page;
+      page.current = 1;
+      page.total = 0;
+      this.setState({
+        filter: data || {},
+        page: page
+      });
+    }
+    window.$http({
+      url: `/admin/inspect/getQuarantineList`,
+      method: 'GET',
+      params: {
+        pageNum: this.state.page.current,
+        pageSize: this.state.page.size
+      }
+    }).then((res) => {
+      if(res && res.data.code == 0) {
+        let list = res.data.data.list;
+        list.map(item => {
+          
+        });
+        let page = this.state.page;
+        page.total = res.data.data.total || 0;
+        this.setState({tableData: list, page: page}, () => {
+          this.setState({
+            loading: false
+          });
+        });
+      } else {
+        this.setState({
+          loading: false
+        });
+      }
+    });
+  }
+
+  // 分页
+  changePageNum = (pageNum) => {
+    let data = Object.assign({}, this.state.page, {current: pageNum})
+    this.setState({page: data}, () => {
+      this.getList();
+    });
+  }
+  changePageSize = (current, size) => {
+    let data = Object.assign({}, this.state.page, {current: 1, size})
+    this.setState({page: data}, () => {
+      this.getList();
+    });
   }
 
   render() {
@@ -49,10 +117,14 @@ class Quarantine extends Component {
     ];
 
     const pagination = {
-      pageSizeOptions: ['10', '20', '50'],
+      pageSizeOptions: ['1', '10', '20', '50'],
       showQuickJumper: true,
       showSizeChanger: true,
-      showTotal: (total) => (`总共 ${total} 条`)
+      showTotal: (total) => (`总共 ${this.state.page.total} 条`),
+      onShowSizeChange: (current, size) => {this.changePageSize(current, size)},
+      onChange: pageNum => { this.changePageNum(pageNum) },
+      current: this.state.page.current,
+      total: this.state.page.total
     }
 
     return (
@@ -62,6 +134,7 @@ class Quarantine extends Component {
           dataSource={ this.state.tableData } 
           pagination={ pagination } 
           bordered 
+          loading={ this.state.loading }
           rowKey={ record => record.id }
         />
       </div>
