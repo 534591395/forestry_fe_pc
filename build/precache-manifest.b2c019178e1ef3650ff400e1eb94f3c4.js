87fc2cf0ed740af6d82ac99e9998d8919c71a83a
hzwangyang1 <hzwangyang1@corp.netease.com>
2020-09-28 11:27:39 +0800

    add

---
+++ build/precache-manifest.b2c019178e1ef3650ff400e1eb94f3c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6685d3f8d3bf45e25313",
+    "url": "/static/css/main.f3d0da75.chunk.css"
+  },
+  {
+    "revision": "6685d3f8d3bf45e25313",
+    "url": "/static/js/main.6685d3f8.chunk.js"
+  },
+  {
+    "revision": "e335284594e7c81aee74",
+    "url": "/static/css/1.d3b8ab60.chunk.css"
+  },
+  {
+    "revision": "e335284594e7c81aee74",
+    "url": "/static/js/1.e3352845.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3867ca4ad76dceb5022fe4b7ce5af811",
+    "url": "/static/media/logo.3867ca4a.png"
+  },
+  {
+    "revision": "401fed54dac7e81ff9ed833f0f4c94c7",
+    "url": "/index.html"
+  }
+];
